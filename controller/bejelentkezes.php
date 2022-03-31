a7fad12b1a6fa4d13b640015d83db219bb2692d9
Klaudia164 <66058455+Klaudia164@users.noreply.github.com>
2022-03-31 12:34:11 +0200

    fail 2.0

--- controller/bejelentkezes.php
+++ controller/bejelentkezes.php
@@ -3,9 +3,9 @@
 require_once "includes/db.php";
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 if(empty(trim($_POST["name"]))){
-    $username_err = "Kérlek írj be egy nevet!."; 
+    $nev_err = "Kérlek írj be egy nevet!."; 
 } else{
-    $username = trim($_POST["name"]);
+    $nev = trim($_POST["name"]);
 }
 
 if(empty(trim($_POST["email"]))){
@@ -15,20 +15,25 @@ if(empty(trim($_POST["email"]))){
 }
 
 if(empty(trim($_POST["date"]))){
-    $date_err = "Kérlek írj be egy dátumot!."; 
+    $szuletes_err = "Kérlek írj be egy dátumot!."; 
 } else{
-    $date = trim($_POST["date"]);
+    $szuletes = trim($_POST["date"]);
 }
 
-if(empty($username_err) && empty($password_err) && empty($date_err)){
+if(empty(trim($_POST["chekbox"]))){
+    $nem_err = "Kérlek válasszon egy nemet!."; 
+}
+if(empty($nev_err) && empty($email_err) && empty($szuletes_err) && empty($nem_err)){
     
 
     $sql = "INSERT INTO jelentkezok (nev, email, szuletes, nem) VALUES (?, ?, ?, ?)";
      
     if($stmt = mysqli_prepare($conn, $sql)){
 
-        $param_username = $username;
-        $param_password = md5($password); 
+        $param_nev = $nev;
+        $param_email = $email;
+        $param_suletes = $szuletes;
+        $param_nem = $nem;
 
         if(mysqli_stmt_execute($stmt)){
 
